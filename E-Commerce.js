52f252826e0f5c45ac10f1d46007f15982cbc545
Levi-0x <achinemerem99@gmail.com>
2025-10-29 17:42:39 +0100

    Finished cart add and remove

--- E-Commerce.js
+++ E-Commerce.js
@@ -1,4 +1,5 @@
 const sideBar = document.querySelector(".shopping-cart")
+const list = document.getElementById("cart-ul")
 
 function slideOut(){
     sideBar.classList.add("open")
@@ -6,4 +7,16 @@ function slideOut(){
 
 function slideClose(){
     sideBar.classList.remove("open")
+}
+
+function addToCart(itemName){
+    const newItem = document.createElement("li")
+    newItem.textContent = itemName
+    list.appendChild(newItem)
+
+    const button = document.createElement("button")
+    button.textContent = "remove"
+    newItem.appendChild(button)
+    button.onclick = () => newItem.remove();
+
 }
