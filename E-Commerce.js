463a49b2141eb938d5d917f75d1f19903be8c060
Levi-0x <achinemerem99@gmail.com>
2025-10-29 16:29:41 +0100

    sidebar E-Commerce page

---
+++ E-Commerce.js
@@ -0,0 +1,9 @@
+const sideBar = document.querySelector(".shopping-cart")
+
+function slideOut(){
+    sideBar.classList.add("open")
+}
+
+function slideClose(){
+    sideBar.classList.remove("open")
+}
