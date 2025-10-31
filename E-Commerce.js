11eba92459a59292e9d99f224b8eaa82d5aca14a
Levi-0x <achinemerem99@gmail.com>
2025-10-31 02:05:02 +0100

    Finished total

--- E-Commerce.js
+++ E-Commerce.js
@@ -1,26 +1,47 @@
-const sideBar = document.querySelector(".shopping-cart")
-const list = document.getElementById("cart-ul")
+    const sideBar = document.querySelector(".shopping-cart")
+    const list = document.getElementById("cart-ul")
+    const total = document.getElementById("total")
+    let amount = 0
 
-function slideOut(){
+    function slideOut(){
     sideBar.classList.add("open")
-}
+    }
 
-function slideClose(){
+    function slideClose(){
     sideBar.classList.remove("open")
-}
+    }
 
-function addToCart(itemName , itemPrice){
+    function addToCart(itemName , itemPrice){
+    update(itemPrice)
+
     const newItem = document.createElement("li")
     newItem.textContent = `${itemName} = $${itemPrice}`
     list.appendChild(newItem)
 
+
     const button = document.createElement("button")
     button.textContent = "remove"
     newItem.appendChild(button)
-    button.onclick = () => newItem.remove();
+    button.onclick = () =>
+    {
+    newItem.remove();
+    remove(itemPrice)     
+    } 
 
-}
+    }
 
-function clearAll(){
+    function remove(itemPrice){
+    amount = amount -= itemPrice
+    total.innerHTML=`$${amount}`
+    }
+
+    function update(itemPrice){
+    amount += itemPrice
+    total.innerHTML=`$${amount}`
+    } 
+
+    function clearAll(){
     list.innerHTML =""
-}
+    total.innerHTML = "$0"
+    amount = 0
+    }
